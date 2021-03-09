336d2418ec47da303eeec78cf12018a6a6c69a67
rauzola <raul_sigoli@hotmail.com>
2021-03-09 01:06:20 -0300

    Propriedades no React

--- Fundamentos do ReactJS/01-github-explorer/src/components/RepositoryList.jsx
+++ Fundamentos do ReactJS/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,10 @@
-const repositoryName = "unform2";
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/'
+}
 
 export function RepositoryList() {
     return (
@@ -6,46 +12,11 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">
-                        Acessar repositórios
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">
-                        Acessar repositórios
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">
-                        Acessar repositórios
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">
-                        Acessar repositórios
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="#">
-                        Acessar repositórios
-                    </a>
-                </li>
+            <RepositoryItem repository={repository} />
+            <RepositoryItem repository={repository} />
+            <RepositoryItem repository={repository} />
+            <RepositoryItem repository={repository} />
+            <RepositoryItem repository={repository} />
             </ul>
         </section>
     )
