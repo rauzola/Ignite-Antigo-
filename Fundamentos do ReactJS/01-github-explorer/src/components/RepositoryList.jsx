4ad21dec2adf04298d02fd4082821777ea679f4a
rauzola <raul_sigoli@hotmail.com>
2021-03-09 15:04:47 -0300

    Listando repositórios

--- Fundamentos do ReactJS/01-github-explorer/src/components/RepositoryList.jsx
+++ Fundamentos do ReactJS/01-github-explorer/src/components/RepositoryList.jsx
@@ -3,11 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/'
-}
 
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
@@ -23,11 +18,9 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     )
