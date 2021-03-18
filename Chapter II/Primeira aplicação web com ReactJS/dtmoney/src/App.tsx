509df873868ee6d26808c21cfa0d80c81fcc2de4
rauzola <raul_sigoli@hotmail.com>
2021-03-17 23:10:17 -0300

    Instalando Styled Components

--- Chapter II/Primeira aplicação web com ReactJS/dtmoney/src/App.tsx
+++ Chapter II/Primeira aplicação web com ReactJS/dtmoney/src/App.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #8257e6;
+`;
+
 export function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Title >Hello World</Title>
     </>
   );
 }
