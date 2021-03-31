5ae576639f065aa883363db3014814fc388e269f
rauzola <raul_sigoli@hotmail.com>
2021-03-31 14:25:37 -0300

    Calculando resumo

--- Chapter II/Primeira aplicação web com ReactJS/dtmoney/src/components/Summary/index.tsx
+++ Chapter II/Primeira aplicação web com ReactJS/dtmoney/src/components/Summary/index.tsx
@@ -9,15 +9,38 @@ import { Container } from "./styles";
 export function Summary() {
     const { transactions } = useContext(TransactionContext);
 
-    return(
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposit += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraw += transaction.amount;
+            acc.total -= transaction.amount;
+
+        }
+        return acc;
+    }, {
+        deposit: 0,
+        withdraw: 0,
+        total: 0,
+    })
+
+    return (
         <Container >
-            
+
             <div>
                 <header>
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposit)}
+
+                </strong>
             </div>
 
             <div>
@@ -25,7 +48,13 @@ export function Summary() {
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Entradas" />
                 </header>
-                <strong>-R$500,00</strong>
+                <strong>-
+                {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                }).format(summary.withdraw)}
+
+                </strong>
             </div>
 
 
@@ -34,7 +63,14 @@ export function Summary() {
                     <p>Total</p>
                     <img src={totalImg} alt="Entradas" />
                 </header>
-                <strong>R$500,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
+
+
+                </strong>
             </div>
         </Container>
     )
