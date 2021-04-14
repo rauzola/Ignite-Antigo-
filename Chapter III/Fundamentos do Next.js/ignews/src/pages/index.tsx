e3846bfa1df52fbb2bf3bf166e1e1444bdeb994b
rauzola <raul.sigoli@anpad.org.br>
2021-04-13 21:12:32 -0300

    Title dinâmico por página

--- Chapter III/Fundamentos do Next.js/ignews/src/pages/index.tsx
+++ Chapter III/Fundamentos do Next.js/ignews/src/pages/index.tsx
@@ -1,10 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/global.module.scss';
-
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      <Head>
+        <title>IG.News</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
+    </>
   )
 }
