88adc3dadfa2f4dca59ec5d41d678c3c8c053910
rauzola <raul_sigoli@hotmail.com>
2021-04-18 14:04:12 -0300

    Estratégias de autenticação

--- Chapter III/Back-end no front-end/ignews/src/pages/api/users.ts
+++ Chapter III/Back-end no front-end/ignews/src/pages/api/users.ts
@@ -1,5 +1,9 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 
+// JWT (Storage)
+// Next Auth (Social) usaremos esse
+// Congnito, Auth8
+
 export default (request: NextApiRequest, response: NextApiResponse) => {
     const users = [
         { id: 1, name: 'Raul' },
