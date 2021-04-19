d89a150df427d29e3d7387f2273e506164320880
rauzola <raul.sigoli@anpad.org.br>
2021-04-19 19:04:03 -0300

    Salvando usuário no banco

--- Chapter III/Back-end no front-end/ignews/src/pages/api/auth/[...nextauth].ts
+++ Chapter III/Back-end no front-end/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb';
+
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     
@@ -10,4 +14,27 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user,account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+
+      
+    },
+  }
 })
