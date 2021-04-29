4c22a4b06df94ac84d615ef61b1455de37136a9f
rauzola <raul_sigoli@hotmail.com>
2021-04-29 00:42:25 -0300

    Componente: ActiveLink

--- Chapter III/Front-end JAMStack/ignews/src/components/Header/index.tsx
+++ Chapter III/Front-end JAMStack/ignews/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Link from 'next/link';
 import { SignInButton } from '../SignInButton';
 import  styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
     return (
@@ -10,12 +10,12 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <Link href="/">
-                      <a  className={styles.active}>Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                      <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink   activeClassName={styles.active} href="/posts" prefetch>
                         <a >Posts</a>
-                    </Link>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
