fcc25dfb967ad533150d637673a57ae1ca7f315e
rauzola <raul_sigoli@hotmail.com>
2021-07-17 18:44:51 -0300

    Criando hook de permissão

--- Chapter IV/Autenticação e autorização/nextauth/src/pages/dashboard.tsx
+++ Chapter IV/Autenticação e autorização/nextauth/src/pages/dashboard.tsx
@@ -1,12 +1,17 @@
 import { useContext, useEffect } from "react"
 import { withSSRAuth } from "../../utils/withSSRAuth";
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from '../services/apiClient';
 
 export default function Dashboard() {
 
-  const { user } = useContext(AuthContext);
+  const { user, isAuthenticated } = useContext(AuthContext);
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  });
 
   useEffect(() => {
     api.get('/me')
@@ -15,7 +20,12 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      { userCanSeeMetrics && <div>Métricas</div>}
+
+    </>
   )
 }
 
